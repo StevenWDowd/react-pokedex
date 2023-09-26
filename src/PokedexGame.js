b35e6d11860877850c62494f16aa3788213e38f3
Steven Dowd <swdowd@gmail.com>
2023-09-25 20:37:58 -0400

    Implemented and styled Pokedex Game

--- src/PokedexGame.js
+++ src/PokedexGame.js
@@ -1,10 +1,17 @@
 import Pokecard from "./Pokecard";
 
 
-function PokedexGame({ pokemon }){
+function PokedexGame({ pokemon, isWinner, exp }){
+  console.log("exp is: ", exp);
+  let result;
+  isWinner ? result = "won" : result = "lost";
   return (
-    <div className="pokedex">
-      {pokemon.map(mon => <Pokecard pokeData={mon}/>)};
+    <div className="pokedexgame">
+      <div className="PokedexGame-exp-field">Total EXP: {exp}</div>
+        <div className="pokedex">
+          {pokemon.map(mon => <Pokecard pokeData={mon}/>)};
+        </div>
+      <div className="PokdexGame-result-field" style={isWinner ? {color: "green"} : {color:"red"}}>You {result}!</div>
     </div>
   )
 
--- src/Pokegame.js
+++ src/Pokegame.js
@@ -5,21 +5,33 @@ import { shufflePokemon, createHands } from "./helpers";
 
 function Pokegame({pokemon}) {
 
+  //console.log("pokemon are ", pokemon);
   const shuffledPokemon = shufflePokemon(pokemon);
   const [firstHand, secondHand] = createHands(shuffledPokemon);
+  console.log("firstHand is ", firstHand);
 
   const firstHandExp = firstHand.reduce((a,c) => {
+    console.log("baseexp is ", c.base_experience);
     return a+c.base_experience;
-  })
+  }, 0);
 
   const secondHandExp = secondHand.reduce((a,c) => {
     return a+c.base_experience;
-  })
+  }, 0);
+
+  console.log("firstHandExp is ", firstHandExp);
+  // let winner;
+
+  // (firstHandExp > secondHandExp) ? winner = firstHand : winner = secondHand;
 
   return (
-    <div className="App">
-      <PokedexGame pokemon={firstHand} exp = {firstHandExp} />
-      <PokedexGame pokemon={secondHand} exp = {secondHandExp}/>
+    <div className="Pokegame">
+      <PokedexGame pokemon={firstHand} exp = {firstHandExp}
+                              isWinner={firstHandExp > secondHandExp}/>
+      <PokedexGame pokemon={secondHand} exp = {secondHandExp}
+                            isWinner={secondHandExp > firstHandExp}/>
     </div>
   );
 }
+
+export default Pokegame;
